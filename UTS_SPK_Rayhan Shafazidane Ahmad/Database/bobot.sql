789186505ea9bdd14dce23de07a4efff72dc45d1
rayhansza <149569401+rayhansza@users.noreply.github.com>
2023-11-01 16:56:00 +0700

    Add files via upload

---
+++ UTS_SPK_Rayhan Shafazidane Ahmad/Database/bobot.sql
@@ -0,0 +1,22 @@
+INSERT INTO public.bobot (nama_monitor,reputasi_brand,refresh_rate,resolusi,harga,ukuran_layar) VALUES
+	 ('Samsung CF390',3,1,1,1,2),
+	 ('Lenovo L24i-30',2,1,1,1,1),
+	 ('Samsung Odyssey G9',3,4,2,4,4),
+	 ('Samsung S24R350',3,1,1,1,2),
+	 ('LG UltraGear 24GN600',1,3,1,1,2),
+	 ('ASUS TUF VG279QM',2,3,1,3,3),
+	 ('AOC 24G2Z',1,3,1,2,1),
+	 ('ZOWIE XL2546K',3,4,1,4,3),
+	 ('Samsung Odyssey G5',3,3,2,3,4),
+	 ('ZOWIE XL2566K',3,5,1,4,3);
+INSERT INTO public.bobot (nama_monitor,reputasi_brand,refresh_rate,resolusi,harga,ukuran_layar) VALUES
+	 ('Samsung CF390',3,1,1,1,2),
+	 ('Lenovo L24i-30',2,1,1,1,1),
+	 ('Samsung Odyssey G9',3,4,2,4,4),
+	 ('Samsung S24R350',3,1,1,1,2),
+	 ('LG UltraGear 24GN600',1,3,1,1,2),
+	 ('ASUS TUF VG279QM',2,3,1,3,3),
+	 ('AOC 24G2Z',1,3,1,2,1),
+	 ('ZOWIE XL2546K',3,4,1,4,3),
+	 ('Samsung Odyssey G5',3,3,2,3,4),
+	 ('ZOWIE XL2566K',3,5,1,4,3);
---
+++ UTS_SPK_Rayhan Shafazidane Ahmad/Database/monitor.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.monitor (nama_monitor,reputasi_brand,refresh_rate,resolusi,harga,ukuran_layar) VALUES
+	 ('Samsung CF390','Sangat Terkenal','60 hz','1920 x 1080',1676847.00,'24"'),
+	 ('Lenovo L24i-30','Cukup Terkenal','75Hz','1920 x 1080',1530000.00,'23.8"'),
+	 ('Samsung Odyssey G9','Sangat Terkenal','240Hz','5120 x 1440',18741000.00,'49"'),
+	 ('Samsung S24R350','Sangat Terkenal','75Hz','1920 x 1080',1394409.00,'24"'),
+	 ('LG UltraGear 24GN600','Kurang Terkenal','144Hz','1920 x 1080',2550000.00,'24"'),
+	 ('ASUS TUF VG279QM','Cukup Terkenal','144Hz','1920 x 1080',4849000.00,'27"'),
+	 ('AOC 24G2Z','Kurang Terkenal','165Hz','1920 x 1080',3579000.00,'23.8"'),
+	 ('ZOWIE XL2546K','Sangat Terkenal','240Hz','1920 x 1080',9068928.00,'27"'),
+	 ('Samsung Odyssey G5','Sangat Terkenal','165Hz','2560 x 1440',5699000.00,'32"'),
+	 ('ZOWIE XL2566K','Sangat Terkenal','360Hz','1920 x 1080',14700350.00,'27');
